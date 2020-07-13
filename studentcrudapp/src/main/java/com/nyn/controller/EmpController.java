7ac185637085f9d954b645e6fd695d6deac24546
Trevor <Trevor@DESKTOP-A2IOU4P>
2020-07-13 09:04:10 +0530

    data connectivity check and modelandview

--- studentcrudapp/src/main/java/com/nyn/controller/EmpController.java
+++ studentcrudapp/src/main/java/com/nyn/controller/EmpController.java
@@ -1,9 +1,44 @@
 package com.nyn.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.nyn.doa.EmployeeDao;
 
 @Controller
 public class EmpController {
+	@Autowired
+	 EmployeeDao dao;
 	
+	@RequestMapping(value="/save" ,method=RequestMethod.POST)
+	public ModelAndView saveEmp(@RequestParam("id") int id, @RequestParam("name") String name, @RequestParam
+			("salary") float salary) {
+		int c = dao.saveEmp(id, name, salary);
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("saveview");
+		if(c>=1) {
+			String out = "data saved"+"   "+"id = "+id+" "+"name =  "+" "+name;
+			
+			mv.addObject("result", out);
+			String res=c+" row affected";
+			System.out.println(res);
+			return mv;
+			
+		}
+		
+		else {
+			
+			String s = "no change occured";
+			mv.addObject("result", s);
+			System.out.println(s);
+		return mv;
+		}
+		
+		
+	}
 
 }
--- studentcrudapp/src/main/java/com/nyn/doa/EmployeeDao.java
+++ studentcrudapp/src/main/java/com/nyn/doa/EmployeeDao.java
@@ -1,5 +1,22 @@
 package com.nyn.doa;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class EmployeeDao {
+	
+	private JdbcTemplate jdbcTemplate;
+	
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+
+
+	public int saveEmp(int id, String name, float salary) {
+		
+		String sql = "insert into employee values('"+id+"','"+name+"','"+salary+"')";
+		return jdbcTemplate.update(sql);
+	}
 
 }
